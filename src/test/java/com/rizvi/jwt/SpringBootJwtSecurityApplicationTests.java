e45ccc437f0ec329b8c9be55c647dbb363dc8cc0
syedi786 <syedrizvidev12@gmail.com>
2023-03-12 20:10:54 -0600

    Initial commit

---
+++ src/test/java/com/rizvi/jwt/SpringBootJwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizvi.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
